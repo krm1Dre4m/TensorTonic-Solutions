9ff7ac7baed774de30ff4a8ed8ba6d9845a2bb73
Krmii <121635085+krm1Dre4m@users.noreply.github.com>
2026-09-07 17:30:27 +0700

    Solve sample-var-std

--- sample-var-std/sample-var-std.py
+++ sample-var-std/sample-var-std.py
@@ -2,8 +2,11 @@ import numpy as np
 
 def sample_var_std(x: list) -> dict:
     x = np.asarray(x, dtype = float)
-    var_list = np.var(x, ddof = 1)
+    mean_arr = np.mean(x)
+    centered = x - mean_arr
+    var_arr = float(np.sum(centered ** 2)) / (np.size(centered) - 1)
+    std_dev = float(var_arr ** 0.5)
     return {
-        "variance": float(var_list),
-        "standard_deviation": float(var_list ** 0.5)
+        "variance": var_arr,
+        "standard_deviation": std_dev
     }
