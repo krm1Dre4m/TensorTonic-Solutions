c70b492e09c509780f151a92296e2b23bb52805b
Krmii <121635085+krm1Dre4m@users.noreply.github.com>
2026-09-08 22:04:01 +0700

    Solve pearson-correlation

--- pearson-correlation/pearson-correlation.py
+++ pearson-correlation/pearson-correlation.py
@@ -1,10 +1,4 @@
 import numpy as np
-
-def covariance(X: np.ndarray, centered: np.ndarray) -> np.ndarray:
-    return centered.T @ centered / (X.shape[0] - 1.0)
-
-def standard_deviation(X: np.ndarray, centered: np.ndarray) -> np.ndarray:
-    return np.sqrt(np.sum(centered ** 2, axis = 0) / (X.shape[0] - 1.0))
     
 def pearson_correlation(X: list) -> np.ndarray:
     """
@@ -14,8 +8,10 @@ def pearson_correlation(X: list) -> np.ndarray:
     X = np.asarray(X, dtype = float)
     centered = X - np.mean(X, axis = 0)
 
-    cov = covariance(X, centered)
-    std_dev = standard_deviation(X, centered)
+    
+
+    cov = centered.T @ centered / (X.shape[0] - 1.0)
+    std_dev = np.sqrt(np.diag(cov))
     outer_matrix = np.outer(std_dev, std_dev)
 
     return np.where(outer_matrix == 0, np.nan, cov / outer_matrix)
