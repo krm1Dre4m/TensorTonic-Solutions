4eb202eac649585b01e11361b3339fb6e0495dbd
Krmii <121635085+krm1Dre4m@users.noreply.github.com>
2026-09-08 22:04:12 +0700

    Solve pearson-correlation

--- pearson-correlation/pearson-correlation.py
+++ pearson-correlation/pearson-correlation.py
@@ -8,8 +8,6 @@ def pearson_correlation(X: list) -> np.ndarray:
     X = np.asarray(X, dtype = float)
     centered = X - np.mean(X, axis = 0)
 
-    
-
     cov = centered.T @ centered / (X.shape[0] - 1.0)
     std_dev = np.sqrt(np.diag(cov))
     outer_matrix = np.outer(std_dev, std_dev)
